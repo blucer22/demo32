86d61d1854a2af03c63e55e2eda1d08868dbf09a
blucer22 <blucer22@126.com>
2018-10-17 14:02:27 +0800

    new commit

--- src/main/java/com/example/demo3/controller.java
+++ src/main/java/com/example/demo3/controller.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
     @RequestMapping( value="/login", method = RequestMethod.GET )
     public String hello12(){
-        return "hello world springboot project1234";
+        return "hello world springboot project123445";
     }
 }
